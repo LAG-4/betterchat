7d09434dce02c2fa4099650439e46af0514cccc8
LAG-4 <aryangupta4feb@gmail.com>
2025-06-06 20:01:59 +0530

    basic structure and not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <>
+        <div className="min-h-screen flex justify-center items-center text-3xl">PAGE NOT FOUND</div>
+        
+        </>
+    )
+}
